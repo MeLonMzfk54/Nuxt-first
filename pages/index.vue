3ab9a5f1656206d994fe5e6c639e4fc823eeb628
Garifullin Anton <garifullin@paylate.info>
2026-03-23 20:28:14 +0700

    feat: add routing

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header>
+      <ul>
+        <li><NuxtLink to="/">Главная</NuxtLink></li>
+        <li><NuxtLink to="/contact">Контакты</NuxtLink></li>
+        <li><NuxtLink to="/users">Пользователи</NuxtLink></li>
+      </ul>
+    </header>
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ pages/users/[user_id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>User with id: {{ user_id }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const {user_id} = useRoute().params;
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
